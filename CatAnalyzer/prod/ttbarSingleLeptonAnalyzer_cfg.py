e5b7dd8ada18f798683fde48483fdf2cd403fcff
arlee <arilee@ui20.sdfarm.kr>
2016-01-21 00:34:51 +0900

    update it

--- CatAnalyzer/prod/ttbarSingleLeptonAnalyzer_cfg.py
+++ CatAnalyzer/prod/ttbarSingleLeptonAnalyzer_cfg.py
@@ -1,11 +1,11 @@
 #------------------------------------------------------------------
 #------------------------------------------------------------------
 # Data or MC Sample
-runOnMC      = False
-# runOnTTbarMC = 0 # No ttbar
-runOnTTbarMC = 1 # ttbar Signal
-# runOnTTbarMC = 2 # ttbar Background
-#runOnTTbarMC = 0
+runOnMC      = True
+# runOnTTbarMC == 0, No ttbar
+# runOnTTbarMC == 1, ttbar Signal
+# runOnTTbarMC == 2, ttbar Background
+runOnTTbarMC = 0
 #------------------------------------------------------------------
 #------------------------------------------------------------------
 
@@ -25,57 +25,39 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #'file:/afs/cern.ch/user/b/brochero/CATTuples_August/v7-3-6/cat74/src/CATTools/CatProducer/prod/catTuple-PUPPI-v7-3-6.root' # -- MC PUPPI (v7-3-6)
-        #'file:/cms/scratch/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v4/150805_203807/0000/catTuple_245.root' # -- MC
-        #'root://cms-xrdr.sdfarm.kr///xrd/store/group/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-3-4_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v4/150810_215031/0000/catTuple_276.root' # -- MC
-        #'file:/cms/home/brochero/CATTuples_August/v7-3-4/cat74/src/CATTools/CatAnalyzer/prod/SingleMu-PromptReco_catTuple_44.root'   # -- DATA
-        #'root://cms-xrdr.sdfarm.kr///xrd/store/group/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-3-6_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v4/150820_215807/0000/catTuple_108.root'    # -- XROOT test
-#        'file:/cms/home/brochero/CATTuples_August/Central-v7-3-6/cat74/src/CATTools/CatAnalyzer/prod/catTuple_108.root' # -- MC
-        #'file:/cms/home/brochero/CATTuples_July/cat74/src/CATTools/CatAnalyzer/catTuple_83.root' # -- Data
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_13.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_14.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_16.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_17.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_18.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_19.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_21.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_23.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_24.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_25.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_26.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_27.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_31.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_35.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_36.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_37.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_38.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_41.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_43.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_45.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_6.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_78.root',
-       '/store/user/tjkim/pf/TT_TuneCUETP8M1_13TeV-powheg-pythia8/PAT2CAT/160116_164927/0000/catTuple_9.root',
-       #'file:catTuple.root'
+        #'root://cms-xrdr.sdfarm.kr:1094///xrd/store/group/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-4-5_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151109_234028/0000/catTuple_1.root'
+        #'root://cms-xrdr.sdfarm.kr:1094///xrd/store/group/CAT/SingleMuon/v7-4-5_Run2015D-05Oct2015-v1/151109_230333/0000/catTuple_1.root'
+        'root://cms-xrdr.sdfarm.kr:1094///xrd/store/group/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-6_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151130_231748/0000/catTuple_1.root'
     )
 )
 
-# json file (Only Data)
-#import FWCore.PythonUtilities.LumiList as LumiList
-#process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt').getVLuminosityBlockRange()
+# PUReWeight
+# process.load("CATTools.CatProducer.pileupWeight_cff")
+# from CATTools.CatProducer.pileupWeight_cff import pileupWeightMap
+# process.pileupWeight.weightingMethod = "RedoWeight"
+# process.pileupWeight.pileupRD = pileupWeightMap["Run2015_25nsV1"]
+# process.pileupWeight.pileupUp = pileupWeightMap["Run2015Up_25nsV1"]
+# process.pileupWeight.pileupDn = pileupWeightMap["Run2015Dn_25nsV1"]
 
-process.TLPJ = cms.EDAnalyzer('TtbarSingleLeptonAnalyzer',
+# json file (Only Data)
+# import FWCore.PythonUtilities.LumiList as LumiList
+# process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_246908-259891_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt').getVLuminosityBlockRange()
+
+process.ttbarSingleLepton = cms.EDAnalyzer('TtbarSingleLeptonAnalyzer',
                                            sampleLabel       = cms.untracked.bool(runOnMC),
                                            TTbarSampleLabel  = cms.untracked.int32(runOnTTbarMC),
-                                           genTopLabel   = cms.InputTag("catGenTops"),
-                                           genLabel      = cms.InputTag("prunedGenParticles"),
-                                           muonLabel     = cms.InputTag("catMuons"),
-                                           electronLabel = cms.InputTag("catElectrons"),
-                                           jetLabel      = cms.InputTag("catJets"),
-                                           metLabel      = cms.InputTag("catMETs"),
-                                           pvLabel       = cms.InputTag("catVertex:nGoodPV"),
-                                           puWeight      = cms.InputTag("pileupWeight"),
-                                           triggerBits = cms.InputTag("TriggerResults::HLT"), # Not working yet
-                                           triggerObjects = cms.InputTag("catTrigger"), # Not working yet
+                                           genWeightLabel    = cms.InputTag("genWeight:genWeight"),
+                                           genLabel          = cms.InputTag("prunedGenParticles"),
+                                           genttbarCatLabel  = cms.InputTag("GenTtbarCategories:genTtbarId"),
+                                           muonLabel         = cms.InputTag("catMuons"),
+                                           electronLabel     = cms.InputTag("catElectrons"),
+                                           jetLabel          = cms.InputTag("catJets"),
+                                           #metLabel         = cms.InputTag("catMETs"),
+                                           metLabel          = cms.InputTag("catMETsNoHF"),
+                                           pvLabel           = cms.InputTag("catVertex:nGoodPV"),
+                                           puWeightLabel     = cms.InputTag("pileupWeight"),
+                                           triggerBits       = cms.InputTag("TriggerResults::HLT"), # Not working yet
+                                           triggerObjects    = cms.InputTag("catTrigger"), # Not working yet
                                            )
 
 process.TFileService = cms.Service("TFileService",
@@ -86,4 +68,6 @@ process.TFileService = cms.Service("TFileService",
 #process.Tracer = cms.Service("Tracer")
 #process.dump=cms.EDAnalyzer('EventContentAnalyzer')
 #process.p = cms.Path(process.demo*process.dump)
-process.p = cms.Path(process.TLPJ)
+# process.p = cms.Path(process.pileupWeight*
+#                      process.ttbarSingleLepton)
+process.p = cms.Path(process.ttbarSingleLepton)
